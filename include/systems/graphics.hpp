feae0c36f6ccd7523fe9a24594addf06824479a9
Flavio Moreira <fhnmor21@gmail.com>
2016-04-10 22:30:51 +0000

    organizing systems include files

--- include/systems/graphics.hpp
+++ include/systems/graphics.hpp
@@ -1,4 +1,28 @@
 /**
+The MIT License (MIT)
+
+Copyright (c) 2016 Flavio Moreira
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
   *
   *
   */
@@ -7,12 +31,12 @@
 #define GRAPHIC_SYSTEMS_HPP
 
 // #include "utils_colour.hpp"
-#include "base/display_interface.hpp"
 #include "components/graphic.hpp"
 #include "components/transform.hpp"
+#include "core/display_interface.hpp"
 #include "systems/interface.hpp"
 
-namespace W2E
+namespace Hydrogine
 {
 
 namespace System
@@ -33,6 +57,7 @@ public:
   Graphics(Graphics& other) = delete;
   virtual ~Graphics();
 
+  // FIXME: replace this by a centralized resource manager... possibly reuse something ready!
   // methods specific to the graphic system
   DisplayHandler createDisplay(const BoxBoundXYWH& rect, Flags flags = 0);
 
@@ -69,6 +94,6 @@ ResourceID Graphics::loadResourceFromFile(const String& filepath)
 
 } // end namespace System
 
-} // end namespace W2E
+} // end namespace Hydrogine
 
 #endif // GRAPHIC_SYSTEMS_HPP
