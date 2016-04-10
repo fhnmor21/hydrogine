403c71683c77b4098a5cc7386f1632d290ba63d9
Flavio Moreira <fhnmor21@gmail.com>
2016-04-10 22:22:19 +0000

    organizing component include files

--- include/components/audio.hpp
+++ include/components/audio.hpp
@@ -1,4 +1,28 @@
 /**
+The MIT License (MIT)
+
+Copyright (c) 2016 Flavio Moreira
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
   * structs containing component data
   *
   */
@@ -6,20 +30,20 @@
 #ifndef COMPONENT_AUDIO_HPP
 #define COMPONENT_AUDIO_HPP
 
-#include "utils/types.hpp"
+#include "core/types.hpp"
 
-namespace W2E
+namespace Hydrogyne
 {
-using namespace Utils;
 
 namespace Component
 {
 
 // forward declaration
+class AudioInterface;
 class MusicData;
 class SoundFxData;
 
-struct Audio
+struct AudioPod
 {
   MusicData* music;
   float musicVolume; // values between 0.0 - 1.0
@@ -27,10 +51,20 @@ struct Audio
   SoundFxData* soundFx;
   float soundFxVolume; // values between 0.0 - 1.0
   eAudioState soundFxState;
+
+  AudioInterface* resource;
+};
+
+class AudioInterface
+{
+public:
+  AudioInterface() {}
+  virtual ~AudioInterface() {}
+  virtual void play(const AudioPod& component) = 0;
 };
 
 } // namespace Component
 
-} // end namespace W2E
+} // end namespace Hydrogyne
 
 #endif // COMPONENT_AUDIO_HPP
